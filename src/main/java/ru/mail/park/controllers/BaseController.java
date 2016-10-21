5b68949cf1e1605736e35f37cb7de8ac7ece0d64
BOG8 <bolshakov888@gmail.com>
2016-10-21 23:52:35 +0300

    BaseController сделан абстрактным + добавлена анотация @RestController

--- src/main/java/ru/mail/park/controllers/BaseController.java
+++ src/main/java/ru/mail/park/controllers/BaseController.java
@@ -1,6 +1,7 @@
 package ru.mail.park.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
@@ -9,7 +10,9 @@ import javax.sql.DataSource;
  * Created by zac on 21.10.16.
  */
 
-public class BaseController {
+
+@RestController
+public abstract class BaseController {
     protected final DataSource dataSource;
 
     @Autowired
